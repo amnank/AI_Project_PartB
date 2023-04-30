beba64413681a265c4e4d9203364e5ce95c4e0a3
Hassan <hassan.aamer03@gmail.com>
2023-04-30 15:49:32 +1000

    Implemented convolutional layers

--- agent/cnn.py
+++ agent/cnn.py
@@ -1,5 +1,52 @@
-from nn import *
+from nn import Layer
 import numpy as np
 
 class Convolutional(Layer):
-    
+    def __init__(self, input_shape, kernel_size, kernel_num):
+        input_depth, input_height, input_width = input_shape
+
+        self.kernel_num = kernel_num
+        self.input_depth = input_depth
+        self.input_shape = input_shape
+
+        self.output_shape = (kernel_num, input_height - kernel_size + 1, input_width - kernel_size + 1)
+        self.kernal_shape = (kernel_num, input_depth, kernel_size, kernel_size)
+
+        self.kernels = np.random.randn(*self.kernels_shape)
+        self.biases = np.random.randn(*self.output_shape)
+
+    def forward(self, layer_input):
+        self.input = layer_input
+        self.output = np.copy(self.biases)
+
+        for i in range(self.kernel_num):
+            for j in range(self.input_depth):
+                self.output[i] += np.correlate(self.input[j], self.kernels[i,j], "valid")
+        
+        return self.output
+
+    def backward(self, d_outputs, learning_rate):
+        d_kernels = np.zeros(*self.kernels_shape)
+        d_biases = d_outputs
+        d_inputs = np.zeros(*self.input_shape)
+
+        for i in range(self.kernel_num):
+            for j in range(self.input_depth):
+                d_kernels[i, j] = np.correlate(self.input[j], d_outputs[i], "valid")
+                d_inputs[j] = np.convolve(d_outputs[i], self.kernels[i, j], "full")
+
+        self.kernels -= learning_rate * d_kernels
+        self.biases -= learning_rate * d_biases
+
+        return d_inputs
+
+class Flatten(Layer):
+    def __init__(self, input_shape, output_shape):
+        self.input_shape = input_shape
+        self.output_shape = output_shape
+
+    def forward(self, layer_input):
+        return np.reshape(layer_input, self.output_shape)
+
+    def backward(self, d_output, learning_rate):
+        return np.reshape(d_output, self.input_shape)
