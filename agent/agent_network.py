7906ced3b82a938d59fe6e713ff824f4a5ae80a3
Hassan <hassan.aamer03@gmail.com>
2023-05-05 20:14:08 +1000

    Added get_policy and get_value methods

--- agent/agent_network.py
+++ agent/agent_network.py
@@ -3,15 +3,21 @@ from cnn import Convolutional, Flatten # pylint: disable=import-error
 from nn import Layer, tanH, Sigmoid # pylint: disable=import-error
 
 sys.path.append("neuralnet")
+
+
 class AgentNetwork:
-    """hyper_params : {is_randomized: Bool
-        load_network: network name
+    """hyper_params =  {is_randomized: Bool,
+        load_network: string name of the network to load,
+        input_depth: Depth of the input sample
     }
     """
     def __init__(self, hyper_params, network_name):
-        
+        self.network_name = network_name
+        self.input_depth = hyper_params["input_depth"]
+
+        # Create network
         self.shared = [
-            Convolutional((14,7,7), 3, 20, "Shared1"),
+            Convolutional((self.input_depth,7,7), 3, 20, "Shared1"),
             tanH(),
             Convolutional((20,5,5), 3, 20, "Shared2"),
             tanH(),
@@ -31,6 +37,7 @@ class AgentNetwork:
 
         self.network = [*self.shared, *self.policy_layers, *self.value_layers]
 
+        # Initialize params in each layer
         if (hyper_params["is_randomized"]):
             for layer in self.network:
                 layer.randomize_params()
@@ -40,8 +47,30 @@ class AgentNetwork:
             for layer in self.network:
                 layer.load_params(hyper_params["load_network"])
 
+    def process_shared(self, input_state):
+        if input_state.shape != (self.input_depth, 7, 7):
+            raise ValueError("Input shape is not what is defined in hyper_param")
 
+        output = input_state
+        for layer in self.shared:
+            output = layer.forward(output)
+        return output
 
+    def get_policy(self, input_state):
+        shared_output = self.process_shared(input_state)
+        
+        output = shared_output
+        for layer in self.policy_layers:
+            output = layer.forward(output)
 
+        return output
+        
+    
+    def get_value(self, input_state):
+        shared_output = self.process_shared(input_state)
+        
+        output = shared_output
+        for layer in self.value_layers:
+            output = layer.forward(output)
 
-
+        return output
