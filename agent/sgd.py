df1ba9d08e922e57cf03ed70cb955e694a3865bf
Aamna Nadeem <74487111+amnank@users.noreply.github.com>
2023-05-09 14:58:16 +1000

    added backward, train, sgd functiont to agent network and modified training examples

--- agent/sgd.py
+++ agent/sgd.py
@@ -3,9 +3,6 @@ from agent_network import AgentNetwork
 
 
 class StochasticGradientDescent:
-    """
-    Updates the parameters of the nn according to SGD.
-    """
 
     def __init__(self, learning_rate=0.01, momentum=0.9):
         self.learning_rate = learning_rate
@@ -19,3 +16,27 @@ class StochasticGradientDescent:
         for i in range(len(params)):
             self.velocities[i] = self.momentum * self.velocities[i] + (1 - self.momentum) * grads[i]
             params[i] -= self.learning_rate * self.velocities[i]
+
+
+
+    def loss_function(pred_value, true_value, pred_policy, true_search_policy, parameters, c=1e-4):
+        """
+        Computes the loss function for AlphaZero self-play reinforcement learning algorithm.
+
+        Args:
+        pred_value: predicted value of the current state by neural network
+        true_value: final reward for the current state
+
+        pred_policy: predicted probability distribution of the next move by neural network
+        true_search_policy: search probabilities from MCTS
+
+        Returns:
+        loss: total loss
+        """
+
+        value_loss = (true_value - pred_value) ** 2
+        policy_loss = -(true_search_policy * np.log(pred_policy))
+        reg_term = c * np.sum([np.sum(param**2) for param in parameters])
+        loss = value_loss + policy_loss + reg_term
+
+        return loss
