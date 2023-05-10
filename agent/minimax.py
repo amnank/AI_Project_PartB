bbef843346a68c64223d82db1d9ddea56b511e04
Aamna Nadeem <74487111+amnank@users.noreply.github.com>
2023-05-11 00:33:48 +1000

    added minimiax structure

--- agent/minimax.py
+++ agent/minimax.py
@@ -1,16 +1,47 @@
+import numpy as np
+
 class Node:
-    
+    def __init__(self) -> None:
+        pass
 
-
+# initial values of alpha and beta 
+MAX, MIN = np.inf, -np.inf
 
 class MiniMax:
+
     def __init__(self, game):
         self.game = game
 
-    def max_value(state, game, alpha, beta):
-        if cutoff_test(state):
-            return eval(state)
+    
+    def get_best_val(self, node, depth, isMaxPlaying, alpha, beta):
         
-        for each child in state.children():
+        # If node is a leaf node
+        if leaf_node(node):
+            return get_value(node)
+        
+        # MAX is playing
+        if isMaxPlaying:
+            best_val = MIN
+            for child in node.children:
+                value = self.get_best_val(child, depth + 1, False, alpha, beta)
+                best_val = max(best_val, value)
+                alpha = max(alpha, best_val)
+
+                if beta <= alpha:
+                    break
+
+            return best_val
+
+        # MIN is playing
+        else:
+            best_val = MAX
+            for node in node.children:
+                value = self.get_best_val(child, depth + 1, True, alpha, beta)
+                best_val = min(best_val, value)
+                beta = min(beta, best_val)
+                if beta <= alpha:
+                    break
+            return best_val
+
 
     
