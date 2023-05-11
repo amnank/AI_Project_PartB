2bdcbbd4e5d8d8d9494a10385ab68a6d48587251
Aamna Nadeem <74487111+amnank@users.noreply.github.com>
2023-05-11 15:17:45 +1000

    added pruning to minimax function and created a wrapper function

--- agent/minimax.py
+++ agent/minimax.py
@@ -4,44 +4,70 @@ class Node:
     def __init__(self) -> None:
         pass
 
-# initial values of alpha and beta 
-MAX, MIN = np.inf, -np.inf
+def is_game_ended(node):
+    pass
 
-class MiniMax:
+
+
+
+# TODO: Need to put the nodes children in the best possible order.
+class MiniMaxPruning:
 
     def __init__(self, game):
         self.game = game
+        
+        # TODO: Set this constant
+        self.initial_depth = 0
+        self.MAX = np.inf
+        self.MIN = -np.inf
+
+    def eval(node) -> int:
+        """
+        The evaluation function. 
+        """
+        pass
 
     
     def get_best_val(self, node, depth, isMaxPlaying, alpha, beta):
         
         # If node is a leaf node
-        if leaf_node(node):
-            return get_value(node)
+        if depth == 0 or is_game_ended(node):
+            return eval(node)
         
         # MAX is playing
         if isMaxPlaying:
-            best_val = MIN
-            for child in node.children:
-                value = self.get_best_val(child, depth + 1, False, alpha, beta)
-                best_val = max(best_val, value)
-                alpha = max(alpha, best_val)
+            max_val = self.MIN
 
+            for child in node.children:
+                eval_value = self.get_best_val(child, depth - 1, False, alpha, beta)
+                max_val = max(max_val, eval_value)
+                alpha = max(alpha, eval_value)
+
+                # pruning
                 if beta <= alpha:
                     break
 
-            return best_val
+            return max_val
 
         # MIN is playing
         else:
-            best_val = MAX
+            min_val = self.MAX
+
             for node in node.children:
-                value = self.get_best_val(child, depth + 1, True, alpha, beta)
-                best_val = min(best_val, value)
-                beta = min(beta, best_val)
+                eval_value = self.get_best_val(child, depth - 1, True, alpha, beta)
+                min_val = min(min_val, eval_value)
+                beta = min(beta, eval_value)
+
+                # pruning 
                 if beta <= alpha:
                     break
-            return best_val
+    
+            return min_val
+        
+    
+    # Wrapper function
+    def minimax(self, node, depth):
+        self.get_best_val(node, depth, True, self.MIN, self.MAX)
 
 
     
